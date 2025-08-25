23face1b5b21bad44e312e82b48654bf720b2295
Den <denspace0@outlook.com>
2025-08-25 06:58:31 +0000

    Vite

---
+++ client/vite.config.ts
@@ -0,0 +1,9 @@
+// client/vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',  // Ensure that output is directed to the `dist` directory
+  },
+  // Add other necessary configurations here
+});
